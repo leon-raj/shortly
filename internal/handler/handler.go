87565d21ba1f6f7145b9c392d4576d706c5452a2
Leon Raj <leonraj1606@gmail.com>
2026-09-11 14:40:05 +0530

    Extracted Validation into it's own file.

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,21 +5,3 @@ import "shortly/internal/store"
 type Handler struct {
 	store *store.Store
 }
-
-const MAXLEN = 32
-
-func isBase62(s string) bool {
-	if len(s) == 0 {
-		return false
-	}
-	for i := 0; i < len(s); i++ {
-		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')) {
-			return false
-		}
-	}
-	return true
-}
-
-func isValidShortCode(s string) bool {
-	return len(s) <= MAXLEN && isBase62(s)
-}
