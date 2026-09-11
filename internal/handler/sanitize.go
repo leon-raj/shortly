8b6bb707eabd4097e4fd2f3a14af70f29edd9e1f
Leon Raj <leonraj1606@gmail.com>
2026-09-11 22:42:51 +0530

    Added function to sanitize URL and return the sanitized URL or return an error.

---
+++ internal/handler/sanitize.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"errors"
+	"net/url"
+)
+
+const DOMAIN = "shortly.uk"
+
+var (
+	ErrInvalidLength = errors.New("invalid URL length")
+	ErrInvalidScheme = errors.New("invalid scheme")
+	ErrMissingHost   = errors.New("missing host")
+	ErrInvalidHost   = errors.New("invalid host")
+)
+
+func sanitizeURL(raw string) (string, error) {
+	if len(raw) == 0 || len(raw) > 2048 {
+		return "", ErrInvalidLength
+	}
+	u, err := url.Parse(raw)
+	if err != nil {
+		return "", err
+	}
+	if !(u.Scheme == "http" || u.Scheme == "https") {
+		return "", ErrInvalidScheme
+	}
+	if u.Host == "" {
+		return "", ErrMissingHost
+	}
+	if u.Hostname() == DOMAIN {
+		return "", ErrInvalidHost
+	}
+	return u.String(), nil
+}
